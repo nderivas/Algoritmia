48b50f4e60f02c59c70c759881c13c099214a71c
Nicolás de Rivas Morillo <nico@MB-Air.local>
2025-02-13 20:23:53 +0100

    Práctica 1 para entregar

--- practica1_842102_843740/ejecutar.sh
+++ practica1_842102_843740/ejecutar.sh
@@ -14,10 +14,32 @@ rm -rf pruebas/prueba[0-9]*.txt.copia
 
 clear
 
-for file in pruebas/prueba[0-9]*.txt; do
+for file in pruebas/prueba[0-9]*L[0-9]*.txt; do
+    L=$(echo "$file" | grep -o "L[0-9]*")
     cp $file $file.copia
-    ./bin/huf -c $file.copia
-    ./bin/huf -d $file.copia.huf
+    echo "\n-- PRUEBA $file"
+    echo "\nTiempo de codificación:"
+    time ./bin/huf -c $file.copia
+    echo "Tiempo de decodificación:"
+    time ./bin/huf -d $file.copia.huf
+    echo "Bytes del archivo original:"
+    wc -c $file.copia
+    echo "Bytes del archivo codificado:"
+    wc -c $file.copia.huf
+    if diff $file $file.copia > /dev/null; then
+        echo "$file - SUPERADO"
+    else
+        echo "$file - FALLIDO"
+    fi
+    echo "\n-- PRUEBA $file LONGITUD ${L:1}"
+    echo "\nTiempo de codificación:"
+    time ./bin/huf -l ${L:1} -c $file.copia
+    echo "Tiempo de decodificación:"
+    time ./bin/huf -l ${L:1} -d $file.copia.huf
+    echo "Bytes del archivo original:"
+    wc -c $file.copia
+    echo "Bytes del archivo codificado:"
+    wc -c $file.copia.huf
     if diff $file $file.copia > /dev/null; then
         echo "$file - SUPERADO"
     else
