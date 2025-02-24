0bab6ec4bb923466f43e3690e4356c82182308ab
Nicolás de Rivas Morillo <nico@MB-Air.local>
2025-02-24 17:27:28 +0100

    Arreglados los acripts

--- practica2_842102_843740/ejecutar.sh
+++ practica2_842102_843740/ejecutar.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 source .venv/bin/activate
 
+rm -rf /tmp/pruebas
+[[ -e /tmp/pruebas ]] || mkdir /tmp/pruebas
+
 echo "Comienzan las pruebas"
 for file in pruebas/test[0-9]*N[0-9]*.bmp; do
     echo "--- Prueba: $file"
     N=$(echo "$file" | grep -o "N[0-9]*")
-    ./codigo/costuras ${N:1} $file $file.out.bmp
+    ./codigo/costuras ${N:1} $file /tmp
 done
