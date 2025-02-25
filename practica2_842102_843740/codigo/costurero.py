6b6bba6022080edb892eb3fc44ae2d627c97f667
Nicolás de Rivas Morillo <843740@unizar.es>
2025-02-25 23:54:42 +0100

    plantilla

---
+++ practica2_842102_843740/codigo/costurero.py
@@ -0,0 +1,51 @@
+# Algoritmia básica - Práctica 2
+# Nicolás de Rivas (843740)
+# Cristina Embid (842102)
+# import numpy as np
+# import math
+# from timer import timer
+
+
+class Costurero:
+    img = None
+    brillos = None
+    energias = None
+    matCosturas = None
+    costura = []
+
+    def __init__(self, path):
+        pass
+        # Leer la imagen y meterla en self.img
+        # Calcular los brillos y meterlos en self.brillos
+        # Calcular las energías y meterlas en self.energias
+        # Inicializar self.matCosturas a np.zeros del tamaño de la imagen
+
+    def calcularBrillos(self):
+        pass
+        # clacula los brillos de todos los pixeles y los guarda en self.brillos
+        # Es solo el for de arriba de nuestra función calcularEnergia de ahora
+
+    def calcularEnergiaPixel(self, fil, col):
+        pass
+        # Calcula la energía de un pixel dado y la guarda en self.energias (HECHA)
+
+    def calcularEnergias(self):
+        pass
+        # Calcula todas la energia y la mete en self.energias
+        # Es solo el for de abajo de nuestra función calcularEnergia de ahora
+
+    def C(self, i, j):
+        pass
+        # Esta para mañana
+
+    def calcularCostura(self):
+        pass
+        # Esta para mañana
+
+    def reconstruirCostura(self):
+        pass
+        # La que hay hecha lit ajusta a clases
+
+    def quitarCostura(self):
+        pass
+        # La que hay hecha lit ajusta a clases
