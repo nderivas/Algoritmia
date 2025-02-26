c3664f1a4bd03b23a17cc61305f2c55b7b778d57
Nicolás de Rivas Morillo <nico@MB-Air.local>
2025-02-26 23:37:17 +0100

    Entrega final prac2

--- practica2_842102_843740/ejecutar.sh
+++ practica2_842102_843740/ejecutar.sh
@@ -17,7 +17,6 @@ for file in pruebas/test[0-9]*N[0-9]*.bmp; do
     echo "--- Prueba: $file"
     echo "--- Prueba: $file" >> tiempos.log
     N=$(echo "$file" | grep -o "N[0-9]*")
-    # ./codigo/costuras ${N:1} $file /tmp
-    ./codigo/costurastest ${N:1} $file /tmp
+    ./codigo/costuras ${N:1} $file /tmp
 done
 echo "Pruebas terminadas los resultados estan en /tmp/pruebas"
