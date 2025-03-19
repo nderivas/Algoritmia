c82d4d08253733da9b13814ce6db44879e3def53
Nicolás de Rivas Morillo <nico@MB-Air.local>
2025-03-19 17:34:24 +0100

    Arreglado el script ejecutar.sh

--- practica3_842102_843740/ejecutar.sh
+++ practica3_842102_843740/ejecutar.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 [[ -f bin/recorridosYumi ]] || make
 
+/bin/rm -rf ./pruebas/*_resultados.txt
 clear
-for f in pruebas/prueba*.txt; do
+
+for f in ./pruebas/prueba*.txt; do
     echo "--- Prueba: $f"
     time bin/recorridosYumi "$f" $f"_resultados.txt"
 done
-echo "Pruebas terminadas"
+echo "Pruebas terminadas\n"
+
+read -p "¿Enseñar resultados por pantalla? (s/n) " respuesta
+[[ $respuesta != "s" ]] && exit 0
+
+for f in ./pruebas/*_resultados.txt; do
+    echo "--- Salida: $f"
+    cat "$f"
+done
